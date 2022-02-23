0a6b76eb48edca994c3f0dcd2346e576b1d51fa8
Larry Kagan <larry@superiocity.com>
2022-02-23 14:19:26 -0500

    Class type declarations

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,32 @@
-// // Exclamation point Tells TS that this will not be null.
-// const anchor = document.querySelector('a')!;
-//
-// console.log(anchor.href);
+// classes
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(client: string, details: string, amount: number) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+
+    format () {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+let invoices: Invoice[] = [];
+// invoices.push(1);
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invOne.client = 'yoshi';
+invTwo.amount = 400;
+
+console.log(invoices);
 
 // const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
