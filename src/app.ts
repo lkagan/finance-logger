35f288abd2482614e82012cc030ebd866210bd48
Larry Kagan <larry@superiocity.com>
2022-02-23 13:12:10 -0500

    TypeScript interaction with the DOM

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+// // Exclamation point Tells TS that this will not be null.
+// const anchor = document.querySelector('a')!;
+//
+// console.log(anchor.href);
+
+// const form = document.querySelector('form')!;
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// console.log(form.children);
+
+// Inputs
+const type = document.querySelector('#type') as HTMLSelectElement;
+const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+
+    console.log(
+        type.value,
+        toFrom.value,
+        details.value,
+        amount.valueAsNumber,
+    );
+})
+
+
