1ad342ba67644c0da3bb96684ddfb1417cff0aba
Larry Kagan <larry@superiocity.com>
2022-02-23 17:18:16 -0500

    Interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,34 @@
+// Interface
+interface IsPerson {
+    name: string;
+    age: number;
+
+    speak(a: string): void;
+    spend(a: number): number;
+}
+
+const me: IsPerson = {
+    name: 'Larry',
+    age: 20,
+
+    speak(text: string): void {
+        console.log(text);
+    },
+
+    spend(amount: number): number {
+        console.log('I spent', amount);
+        return amount;
+    },
+};
+
+const greetPerson = (person: IsPerson) => {
+    console.log('hello ', person.name);
+}
+
+greetPerson(me);
+
+console.log(me);
+
 import { Invoice } from './classes/Invoice.js';
 
 const invOne = new Invoice('mario', 'work on the mario website', 250);
