1a889a4e7a38ceb17472815695fe5213a7a0b02f
Larry Kagan <larry@superiocity.com>
2022-02-26 18:33:30 -0500

    Polymorphism in typescript

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,7 @@
 import { Invoice } from './classes/Invoice.js';
 import { Payment } from './classes/Payment.js';
-import { HasFormatter } from "./interfaces/HasFormatter";
-
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-//
-// docOne = new Invoice('yoshi', 'web work', 250);
-// docTwo = new Payment('mario', 'plumbing work', 200);
-//
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-//
-// console.log(docs);
-//
-// const invOne = new Invoice('mario', 'work on the mario website', 250);
-// const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-//
-// let invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
+import { HasFormatter } from "./interfaces/HasFormatter.js";
+import {ListTemplate} from "./classes/ListTemplate.js";
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
@@ -28,6 +10,9 @@ const type = document.querySelector('#type') as HTMLSelectElement;
 const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
+
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
 
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
@@ -40,7 +25,7 @@ form.addEventListener('submit', (e: Event) => {
         doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
     }
 
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 })
 
 
