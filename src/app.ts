6f65f9dbda7b799ac863bcd28e99cf91d2a2c964
Larry Kagan <larry@superiocity.com>
2022-02-26 15:06:07 -0500

    Implement interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,49 +1,27 @@
-// Interface
-interface IsPerson {
-    name: string;
-    age: number;
-
-    speak(a: string): void;
-    spend(a: number): number;
-}
-
-const me: IsPerson = {
-    name: 'Larry',
-    age: 20,
-
-    speak(text: string): void {
-        console.log(text);
-    },
-
-    spend(amount: number): number {
-        console.log('I spent', amount);
-        return amount;
-    },
-};
-
-const greetPerson = (person: IsPerson) => {
-    console.log('hello ', person.name);
-}
-
-greetPerson(me);
-
-console.log(me);
-
 import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+import { HasFormatter } from "./interfaces/HasFormatter";
 
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+//
+// docOne = new Invoice('yoshi', 'web work', 250);
+// docTwo = new Payment('mario', 'plumbing work', 200);
+//
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
+//
+// console.log(docs);
+//
+// const invOne = new Invoice('mario', 'work on the mario website', 250);
+// const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+//
+// let invoices: Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
 
-let invoices: Invoice[] = [];
-// invoices.push(1);
-invoices.push(invOne);
-invoices.push(invTwo);
-
-invoices.forEach(inv => console.log(inv.client, inv.amount, inv.format()))
-
-// const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-// console.log(form.children);
 
 // Inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
@@ -54,12 +32,15 @@ const amount = document.querySelector('#amount') as HTMLInputElement;
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
 
-    console.log(
-        type.value,
-        toFrom.value,
-        details.value,
-        amount.valueAsNumber,
-    );
+    let doc: HasFormatter;
+
+    if (type.value === 'invoice') {
+        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+    } else {
+        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+    }
+
+    console.log(doc);
 })
 
 
