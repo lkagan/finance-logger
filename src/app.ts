198a32f3367b21b67870ef0a2351fd99e2cbcfb8
Larry Kagan <larry@superiocity.com>
2022-03-04 15:46:11 -0500

    Generics

--- src/app.ts
+++ src/app.ts
@@ -28,4 +28,26 @@ form.addEventListener('submit', (e: Event) => {
     list.render(doc, type.value, 'end');
 })
 
+// Generics
+const addUID = <T extends {name: string}>(obj: T) => {
+    let uid = Math.floor(Math.random() * 100);
+    return {...obj, uid};
+}
 
+let docOne = addUID({name: 'yoshi', age: 40});
+// let docTwo = addUID('hello');
+
+console.log(docOne.name);
+
+// with interfaces
+interface Resource<T> {
+    uid: number;
+    resourceName: string;
+    data: T;
+}
+
+const docThree: Resource<object> = {
+    uid: 1,
+    resourceName: 'person',
+    data: {name: 'larry', age: 102}
+}
